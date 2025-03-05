c6f427a673963757ab2f15892019d05ee19a3a64
souhirkaroui <souhir.karoui.ks@gmail.com>
2025-03-05 22:35:00 +0100

    Update AuthentifcationVerifEmailApplicationTests.java

--- Authentifcation_Verif_Email/src/test/java/com/nejib/authentifcation_verif_email/AuthentifcationVerifEmailApplicationTests.java
+++ Authentifcation_Verif_Email/src/test/java/com/nejib/authentifcation_verif_email/AuthentifcationVerifEmailApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@TestPropertySource(properties = "spring.datasource.url=jdbc:h2:mem:testdb")
 class AuthentifcationVerifEmailApplicationTests {
 
     @Test
