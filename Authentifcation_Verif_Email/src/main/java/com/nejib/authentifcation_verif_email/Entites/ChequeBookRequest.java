c6c08b2cb17d8e3dc22a80718fea008f7606b64a
souhirkaroui <souhir.karoui.ks@gmail.com>
2025-03-07 00:15:02 +0100

    Update ChequeBookRequest.java

--- Authentifcation_Verif_Email/src/main/java/com/nejib/authentifcation_verif_email/Entites/ChequeBookRequest.java
+++ Authentifcation_Verif_Email/src/main/java/com/nejib/authentifcation_verif_email/Entites/ChequeBookRequest.java
@@ -1,6 +1,14 @@
 package com.nejib.authentifcation_verif_email.Entites;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
 import lombok.Getter;
 import lombok.Setter;
 
